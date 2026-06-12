d2283e0912efd8d2cf91af81dec4ce409e5b7703
olesimple <kaharafredrick@gmail.com>
2026-06-12 16:48:55 +0300

    authentication patch

--- auth.ts
+++ auth.ts
@@ -1,9 +1,18 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
+const githubClientId = process.env.GITHUB_ID;
+const githubClientSecret = process.env.GITHUB_SECRET;
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: githubClientId || '',
+      clientSecret: githubClientSecret || '',
+    }),
+  ],
   session: { strategy: 'jwt', maxAge: 60 * 60 * 24 },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {},
 });
 
